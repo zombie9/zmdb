6e05d65b3304fcb345054eca2d467065bc4d2fcb
zombie9 <darryl.banks@zombie9.com>
2021-07-01 15:08:39 +0100

    change schema and create add-mutations

--- schema.js
+++ schema.js
@@ -1,7 +1,6 @@
 const axios = require('axios')
 const {
   GraphQLObjectType,
-  GraphQLObject,
   GraphQLString,
   GraphQLInt,
   GraphQLSchema,
@@ -13,18 +12,33 @@ const {
 const DirectorType = new GraphQLObjectType({
   name: 'director',
   fields: () => ({
+    id: { type: GraphQLInt },
     firstName: { type: GraphQLString },
-    lastName: { type: GraphQLString }
+    lastName: { type: GraphQLString },
+    movies: {
+      type: new GraphQLList(MovieType),
+      resolve: (director) => {
+        return axios.get(`http://localhost:3000/movies?directorId=${director.id}`)
+        .then(res => res.data)
+      }
+    }
   })
 })
 
 const MovieType = new GraphQLObjectType({
   name: 'movie',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLInt },
     title: { type: GraphQLString },
-    director: { type: DirectorType },
-    year: { type: GraphQLInt }
+    directorId: { type: GraphQLInt },
+    year: { type: GraphQLInt },
+    director: {
+      type: DirectorType,
+      resolve: (movie) => {
+        return axios.get(`http://localhost:3000/directors/${movie.directorId}`)
+        .then(res => res.data)
+      }
+    }
   })
 })
 
@@ -34,7 +48,7 @@ const RootQuery = new GraphQLObjectType({
     movie: {
       type: MovieType,
       args: {
-        id: { type: GraphQLString }
+        id: { type: GraphQLInt }
       },
       resolve(parentValue, args) {
         const url = `http://localhost:3000/movies/${args.id}`
@@ -53,7 +67,7 @@ const RootQuery = new GraphQLObjectType({
     moviesByYear: {
       type: new GraphQLList(MovieType),
       args: {
-        year: { type: GraphQLString }
+        year: { type: GraphQLInt }
       },
       resolve(parentValue, args) {
         const url = `http://localhost:3000/movies?year=${args.year}`
@@ -62,15 +76,60 @@ const RootQuery = new GraphQLObjectType({
         .then(res => res.data)
       }
     },
-    moviesByDirector: {
-      type: new GraphQLList(MovieType),
+    director: {
+      type: DirectorType,
       args: {
-        lastName: { type: GraphQLString }
+        id: { type: GraphQLInt }
       },
       resolve(parentValue, args) {
-        const url = `http://localhost:3000/movies?director.lastName=${args.lastName}`
+        const url = `http://localhost:3000/directors/${args.id}`
         console.log(url)
         return axios.get(url)
+        .then(res => res.data)
+      }
+    },
+    directors: {
+      type: new GraphQLList(DirectorType),
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/directors/`)
+        .then(res => res.data)
+      }
+    }
+  }
+})
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addMovie: {
+      type: MovieType,
+      args: {
+        title: { type: new GraphQLNonNull(GraphQLString) },
+        directorId: { type: new GraphQLNonNull(GraphQLInt) },
+        year: { type: new GraphQLNonNull(GraphQLInt) }
+      },
+      resolve(parentValue, args) {
+        const url = 'http://localhost:3000/movies/'
+        return axios.post(url, {
+          title: args.title,
+          directorId: args.directorId,
+          year: args.year
+        })
+        .then(res => res.data)
+      }
+    },
+    addDirector: {
+      type: DirectorType,
+      args: {
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        lastName: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parentValue, args) {
+        const url = 'http://localhost:3000/directors/'
+        return axios.post(url, {
+          firstName: args.firstName,
+          lastName: args.lastName,
+        })
         .then(res => res.data)
       }
     }
@@ -79,5 +138,6 @@ const RootQuery = new GraphQLObjectType({
 
 
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation
 })
