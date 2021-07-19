1e423a7134ae495999b7642a13198ad3b0287ac3
zombie9 <darryl.banks@zombie9.com>
2021-07-20 00:12:58 +0100

    add pagination

--- client/src/components/movies.js
+++ client/src/components/movies.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import Movie from './movie'
 import { MOVIES_QUERY } from '../queries'
 import { Spinner } from 'react-bootstrap'
 import { filterList } from '../reactive/variables'
 import { useReactiveVar } from '@apollo/client'
+import { RESULTS_PER_PAGE } from '../constant'
 
 const Movies = () => {
   const currentFilters = useReactiveVar(filterList)
+  const [currentPage, setCurrentPage] = useState(1)
 
   const { loading, error, data } = useQuery(MOVIES_QUERY, {
     fetchPolicy: 'cache-first'
@@ -28,13 +30,40 @@ const Movies = () => {
       })
     }
   })
+
+  const pointer = {
+    cursor: "pointer"
+  }
+
+  const totalResults = filteredData.length
+  const maxPage = Math.ceil(totalResults / RESULTS_PER_PAGE)
+  const endResult = RESULTS_PER_PAGE * currentPage
+  const paginatedData = filteredData.slice((endResult - RESULTS_PER_PAGE), endResult)
+
+  const incrementPage = () => {
+    currentPage < maxPage && setCurrentPage(currentPage + 1)
+  }
+
+  const decrementPage = () => {
+    currentPage > 1 && setCurrentPage(currentPage - 1)
+  }
  
   return (
-    <div className="row card-group row-cols-2 row-cols-md-4 row-cols-lg-5 g-4">
-      {filteredData.map(movie => (
-        <Movie key={movie.id} movie={movie} />
-      ))}
-    </div>
+    <>
+      <div className="row card-group row-cols-2 row-cols-md-4 row-cols-lg-5 g-2 g-md-4">
+        {paginatedData.map(movie => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
+      </div>
+      <div className="d-flex flew-row justify-content-between mt-3">
+        <h3 className={currentPage <= 1 ? "text-muted pe-none" : "text-warning"}>
+          <i style={pointer} className="bi-arrow-left-circle" onClick={decrementPage}></i>
+        </h3>
+        <h3 className={currentPage >= maxPage ? "text-muted pe-none" : "text-warning"}>
+          <i style={pointer} className="bi-arrow-right-circle" onClick={incrementPage}></i>
+        </h3>
+      </div>
+    </>
   )
 }
 
--- client/src/constant/index.js
+++ client/src/constant/index.js
@@ -1,2 +1,3 @@
 export const TMDB_POSTER = 'http://image.tmdb.org/t/p/w500'
 export const TMDB_BACKDROP = 'https://image.tmdb.org/t/p/original'
+export const RESULTS_PER_PAGE = 10
